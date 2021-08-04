4c1fbb82a4c1446d35e8502343ca3bbde8909e13
omama zainab <omamazainab00@gmail.com>
2021-08-04 16:39:49 +0500

    chore: done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,71 @@
 import { useState } from 'react';
 import marked from 'marked';
-import Prism from 'prismjs';
+
 import './App.css';
 
 function App() {
-  const initialState = '';
-  
-  const [content, setContent] = useState(initialState);
+  const placeHolder = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`;
+  const [content, setContent] = useState(placeHolder);
+
 
 
   return (
     <>
-      <textarea id="editor" onChange={(e) => {
-        setContent(marked(e.target.value));
-      }}></textarea>
-      <div id="preview" dangerouslySetInnerHTML={{ __html: content}}></div>
+      <div id="wrapper">
+
+        <textarea id="editor"
+          value={content}
+          onChange={(e) => {
+            setContent(e.target.value);
+          }}></textarea>
+        <div id="preview"
+          dangerouslySetInnerHTML={{ __html: marked(content) }}
+        >
+        </div>
+      </div>
     </>
   );
 }
